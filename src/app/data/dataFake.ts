db3e4390e07e9b88f59fa4ed374973e97e331aaf
sandrabressan <bressan.sandra@yahoo.com.br>
2023-10-14 21:42:12 -0300

    projeto atualizado

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "ACLAMADA SÉRIE ALEMÃ DA NETFLIX",
+    "description": "Junto ao público, o reconhecimento veio na forma da elevada audiência e também em uma votação popular com mais de 2 milhões de pessoas realizada no conceituado site Rotten Tomatoes, quando Dark derrotou nomes fortíssimos como Stranger Things, The Crown, Peaky Blinders e Black Mirror.",
+    "photoCover": "https://farofageek.com.br/wp-content/uploads/2020/06/wallpaper-de-dark-para-celular-800x445.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Stranger Things 5: Will Byers terá papel crucial na última temporada",
+    "description": "Os showrunners contaram que Will Byers, personagem interpretado por Noah Schnapp, será o foco principal dessa nova leva de episódios, cujo enredo conectará às temporadas anteriores, encerrando a história da série."+
+    " Um dos principais motivos para a importância de Will neste capítulo final de Stranger Things se deve ao fato de que ele foi a primeira pessoa a desaparecer para uma dimensão alternativa, conhecida como Mundo Invertido, o que conduz toda a trama da primeira season.",
+    "photoCover": "https://blog.opinionbox.com/wp-content/uploads/2016/08/Stranger-Things-2.png"
+  }
+]
